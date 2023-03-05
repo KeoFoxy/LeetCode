6fc0b74edef65dc29b98647fa31824c24472c704
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2023-03-05 14:04:55 +0300

    Update 0002-add-two-numbers.ts

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -17,3 +17,24 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null, carry: number =
     carry = parseInt(total / 10 + '');
     return new ListNode(total % 10, addTwoNumbers(l1?.next, l2?.next, carry));
 };
+
+
+/*
+JavaScript solution but leet code does not accept reverse() and reduce() function due to compilers difference.
+Leet code uses 16.2 LTS meanwhile mine is 19.6.
+
+This thing works properly.
+
+var addTwoNumbers = function (l1, l2) {
+    l1.reverse().reduce((curr, next) => {
+        return curr + next + ""
+    })
+
+    l2.reverse().reduce((curr, next) => {
+        return curr + next + ""
+    })
+
+    return (l1.join('') * 1 + l2.join('') * 1).toString().split('')
+};
+
+*/
