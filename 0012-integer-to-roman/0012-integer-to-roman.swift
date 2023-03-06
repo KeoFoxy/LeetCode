ebfddc937415cb498468ff500e210f47f243334d
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2023-03-06 09:27:30 +0300

    Time: 21 ms (83.64%), Space: 13.9 MB (76.00%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.swift
@@ -0,0 +1,20 @@
+class Solution {
+    
+    private let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    private let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    
+    func intToRoman(_ num: Int) -> String {
+        
+        var int = num
+        var sym = ""
+        
+        while int > 0 {
+            for (i, d) in values.enumerated() where int - d >= 0 {
+                int -= d
+                sym += symbols[i]
+                break
+            }
+        }
+        return sym
+    }
+}
