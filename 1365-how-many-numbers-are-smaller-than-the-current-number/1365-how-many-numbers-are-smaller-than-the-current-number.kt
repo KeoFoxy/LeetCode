8cf0fceb78963930fa1abbbf6d1a43a5af6e176e
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2023-08-15 14:54:03 +0300

    Time: 267 ms (5.10%), Space: 37.8 MB (20.41%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.kt
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.kt
@@ -1,22 +1,7 @@
 class Solution {
     fun smallerNumbersThanCurrent(nums: IntArray): IntArray {
-
-    val sorted = nums.withIndex().sortedBy { it.value }
-
-    val result = mutableMapOf<Int, Int>()
-    var prevCount = -1  
-    var prevNum = -1    
-
-    for ((count, pair) in sorted.withIndex()) {
-        if (pair.value != prevNum) {
-            result[pair.index] = count
-            prevCount = count
-        } else {
-            result[pair.index] = prevCount
-        }
-        prevNum = pair.value
-    }
-
-    return nums.indices.map { result[it]!! }.toIntArray()
+        return nums.map { value -> 
+            nums.filter { num -> num < value}.size
+        }.toIntArray()
     }
 }
