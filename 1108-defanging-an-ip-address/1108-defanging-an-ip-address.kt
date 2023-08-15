4fc83267e0a8780db217144d33590565cb2dab6f
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2023-08-15 13:57:03 +0300

    Time: 155 ms (62.07%), Space: 33.1 MB (97.41%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.kt
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.kt
@@ -1,5 +1,15 @@
 class Solution {
     fun defangIPaddr(address: String): String {
-        return address.replace(".", "[.]")
+        // return address.replace(".", "[.]")
+        var resultString = StringBuilder("")
+        
+        for(i in 0..address.length - 1){
+            if(address[i] == '.')
+                resultString.append("[.]")
+            else 
+                resultString.append(address[i])
+        }
+        
+        return resultString.toString()
     }
 }
